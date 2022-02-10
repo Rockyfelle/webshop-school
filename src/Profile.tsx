b5b4acc35e2b44f2dc1b0b11f8b1ba0ec511ec39
Rockyfelle <rockyfelle@live.se>
2022-02-10 13:15:37 +0100

    no sem3

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,9 +1,4 @@
 import React, { useState } from 'react';
-import {
-	Form,
-	Message,
-	Modal,
-} from 'semantic-ui-react';
 
 
 
@@ -25,62 +20,7 @@ function Staff(props: any) {
 	}
 
 	return (
-		<Modal
-			size="large"
-			onClose={props.canceled}
-			open={true}
-		>
-			<Modal.Header>Profile</Modal.Header>
-			<Modal.Content image>
-				<Modal.Description>
-					{error.length > 0 &&
-						<Message
-							color="red"
-							content={error}
-						/>
-					}
-					{!loggedIn &&
-						<Form>
-							<Form.Input
-								label="Username"
-								placeholder="Username"
-								value={username}
-								onChange={(e) => setUsername(e.target.value)}
-							/>
-							<Form.Input
-								label="Password"
-								placeholder="Password"
-								value={password}
-								onChange={(e) => setPassword(e.target.value)}
-							/>
-							<Form.Button
-								color="green"
-								fluid
-								onClick={(e) => tryLogin(e)}
-							>
-								Log In
-							</Form.Button>
-						</Form>
-					}
-					{!!loggedIn &&
-						<Form>
-							<Form.Input
-								label="Full Name"
-								value="Mikael Vaihela"
-								onChange={() => {}}
-							/>
-							<Form.Input
-								label="Address"
-								value="Jensen Basement"
-								onChange={() => {}}
-							/>
-						</Form>
-					}
-				</Modal.Description>
-			</Modal.Content>
-			<Modal.Actions>
-			</Modal.Actions>
-		</Modal >
+		<></>
 	);
 }
 

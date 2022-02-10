10e62e9dc7ec742081b8923fdb601838c4b5679c
Rockyfelle <rockyfelle@live.se>
2022-02-10 13:17:26 +0100

    no sem5

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Input, Menu } from 'semantic-ui-react'
+
 
 function App() {
 	const [activeItem, setActiveItem] = useState('home');
@@ -12,33 +12,7 @@ function App() {
 	}
 
 	return (
-		<Menu secondary inverted>
-			<Menu.Item
-				name='home'
-				active={activeItem === 'home'}
-				onClick={(e) => handleItemClick(e, 'home')}
-			/>
-			<Menu.Item
-				name='messages'
-				active={activeItem === 'messages'}
-				onClick={(e) => handleItemClick(e, 'messages')}
-			/>
-			<Menu.Item
-				name='friends'
-				active={activeItem === 'friends'}
-				onClick={(e) => handleItemClick(e, 'friends')}
-			/>
-			<Menu.Menu position='right'>
-				<Menu.Item>
-					<Input icon='search' placeholder='Search...' />
-				</Menu.Item>
-				<Menu.Item
-					name='logout'
-					active={activeItem === 'logout'}
-					onClick={(e) => handleItemClick(e, 'logout')}
-				/>
-			</Menu.Menu>
-		</Menu>
+		<></>
 	)
 }
 
